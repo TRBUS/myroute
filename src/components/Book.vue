839cf51cd1bca80074d74bd5d4b35324a5828263
TRBUS <tr-bus@foxmail.com>
2021-12-30 22:09:54 +0800

    ch14.2 动态路由匹配

---
+++ src/components/Book.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>图书ID：{{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import News from '../components/News'
+import Books from '../components/Books'
+import Videos from '../components/Videos'
+import Book from '../components/Book'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/news',
+    component: News
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/books',
+    component: Books
+  },
+  {
+    path: '/videos',
+    component: Videos
+  },
+  {
+    path: '/book/:id',
+    component: Book
   }
 ]
 
