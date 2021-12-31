693bd046117410aebac241aaaa87ca03ef078553
TRBUS <tr-bus@foxmail.com>
2021-12-31 19:59:15 +0800

    ch14_14-8-1-1_登录验证

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/news">新闻</router-link>
       <router-link to="/books">图书</router-link>
       <router-link to="/videos">视频</router-link>
+      <router-link to="/login">登录</router-link>
     </p>
     <router-view></router-view>
   </div>
